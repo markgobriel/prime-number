583b68abac6a4c77dc1ac36c37183f45d2aec182
Mark Gobriel <markgobriel@gmail.com>
2022-10-13 18:20:00 -0400

    Create primenumber.py
    This is an application I made using python during my Church Coding Club, as a problem-solving task.
    
    You simply enter a number and the computer tells you whether that number is prime or not.

---
+++ primenumber.py
@@ -0,0 +1,20 @@
+# WHAT IS A PRIME NUMBER?
+# a prime number is a number that is only divisble by itself and by 1
+# e.g.: 3, 5, 7, 11, 13, 17 ...
+# 0 and 1 however, are NOT prime numbers
+
+num = int(input("enter your number here"))
+
+if num > 1:
+
+    for i in range(2,num):
+        if (num % i == 0):
+            print(num, "is not a prime number")
+            break
+
+    else:
+        print(num, "is a prime number")
+
+else: 
+    print(num, "is not a prime number")
+    
